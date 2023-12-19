9d2aeff6bc6cd6c326240e99359668e63c8d4fb7
NR Habibi <nrhbb14@gmail.com>
2023-12-19 15:20:32 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold h-screen w-full flex justify-center items-center">vue starter ts + tw</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
